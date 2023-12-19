8ece1f4591a3543e542199a6999041fe458de378
Taeuk <taeuk@Taeukui-MacBookAir.local>
2023-12-19 23:00:41 +0900

    login in backend

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,14 +1,44 @@
 "use strict";
 
-const home = (req, res) => {
-    res.render("home/index");
+
+
+const output = {
+
+    home : (req, res) => {
+        res.render("home/index");
+    },
+    
+    login : (req, res) => {
+        res.render("home/login");
+    },
 };
 
-const login = (req, res) => {
-    res.render("home/login");
+const users = {
+    id: ["kim","lee","park"],
+    psword: ["1234","12345","123456"],
+};
+
+const process = {
+    login: (req,res) => {
+        const id = req.body.id,
+        psword = req.body.psword;
+
+        if(users.id.includes(id)) {
+            const idx = users.id.indexOf(id);
+            if(users.psword[idx] === psword) {
+                return res.json({
+                    success: true,
+                });
+            }
+        }
+        return res.json({
+            success:false,
+            msg: "Failed to login",
+        });
+    },
 };
 
 module.exports = {
-    home,
-    login,
+    output,
+    process,
 };
